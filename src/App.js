0ca3b6f2c1f87d941df1c5d78f9608faad89c739
N1ceK1d <75424060+N1ceK1d@users.noreply.github.com>
2022-12-12 23:45:42 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import Products from './components/Products';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <img className='catalog icon' src="./images/burger.png" />
+        <h1>Products Space</h1>
+        <div className='search-block'>
+          <input placeholder="Искать здесь..." type="search"/>
+          <button type="submit">Поиск</button>
+        </div>
+        <div className='menu login'>
+          <p>Войти</p>
+          <img className="icon login-icon" src="./images/profile.png"/>
+        </div>
+        <div className='menu favorite'>
+          <p>Избранное</p>
+          <img className="icon favorite-icon" src="./images/favorite.png"/>
+        </div>
+        <div className="menu shopping-cart">
+          <p>Корзина</p>
+          <img className="icon shopping-cart-icon" src="./images/shoppingCart.png"/>
+        </div>
+      </header>
+      <Products/>  
+      <footer>
+        <p>Copyright by N1ceK1d</p>
+      </footer>
+    </div>
+  
+  )
+}
+
+export default App;
